3d30e96c206c9d28e46fd84a8a248aa1e7012f7d
Alexander Boneham <alexboneham@gmail.com>
2022-02-22 10:27:32 -0500

    add Async Error wrapper function and basic Express error handler

---
+++ utils/catchAsync.js
@@ -0,0 +1,11 @@
+module.exports = func => {
+    return (req, res, next) => {
+        func(req, res, next).catch(next);
+    }
+}
+
+// Logic is as follows:
+//     return a function,
+//     that accepts a function,
+//     and executes that function,
+//     but catches any errors and passes them to next()
