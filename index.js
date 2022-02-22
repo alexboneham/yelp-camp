60fcf8751d3de89007c7a4c6199925b251320786
Alexander Boneham <alexboneham@gmail.com>
2022-02-22 15:37:34 -0500

    add Joi validation
    add Joi package;
    create schema with specifics of validation;
    Handle error from Joi by mapping over error details array to create message.

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+const Joi = require('joi');
+const { join } = require('path');
 
 
 // Connect mongoose database
@@ -50,7 +52,22 @@ app.get('/campgrounds/new', (req, res) => {
 
 // Create new campground in database
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    // Define Joi schema for campground
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required()
+        }).required()
+    })
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400);
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -91,7 +108,7 @@ app.all('*', (req, res, next) => {
 // Add Express Error handler
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
-    if(!err.message) err.message = 'Oh No, Something Went Wrong!';
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!';
     res.status(statusCode).render('error', { err })
 })
 
