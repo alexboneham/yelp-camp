a06fab33c6d0ef1162048f66a227e04a333ea969
Alexander Boneham <alexboneham@gmail.com>
2022-04-15 17:29:59 -0400

    Restructure campground routes
    Move campgrounds routes to a separate file. Add path to the routes in index.js file. 
    Fix typo in campgrounds index view.

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const { campgroundSchema, reviewSchema } = require("./schemas");
 const Review = require("./models/review");
 const { isError } = require("joi");
 
+const campgrounds = require("./routes/campgrounds");
+
 // Connect mongoose database
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
@@ -36,15 +38,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 // Define Joi validator middleware
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
 
 const validateReview = (req, res, next) => {
   const { error } = reviewSchema.validate(req.body);
@@ -61,74 +54,8 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get(
-  "/campgrounds",
-  catchAsync(async (req, res) => {
-    // Display all campgrounds
-    const campgrounds = await Campground.find({});
-    res.render("campgrounds/index", { campgrounds });
-  })
-);
-
-// Serve route new campground
-app.get("/campgrounds/new", (req, res) => {
-  res.render("campgrounds/new");
-});
-
-// Create new campground in database
-app.post(
-  "/campgrounds",
-  validateCampground,
-  catchAsync(async (req, res, next) => {
-    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-// Serve route to edit campground
-app.get(
-  "/campgrounds/:id/edit",
-  catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render("campgrounds/edit", { campground });
-  })
-);
-
-// PUT route to update campground edit
-app.put(
-  "/campgrounds/:id",
-  validateCampground,
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(
-      id,
-      { ...req.body.campground },
-      { new: true }
-    );
-    // Used spread here in vid, not sure why. Also don't need new: true
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-app.delete(
-  "/campgrounds/:id",
-  catchAsync(async (req, res) => {
-    await Campground.findByIdAndDelete(req.params.id);
-    res.redirect("/campgrounds");
-  })
-);
-
-app.get(
-  "/campgrounds/:id",
-  catchAsync(async (req, res) => {
-    // Show page for singular campgrounds
-    const { id } = req.params;
-    const campground = await Campground.findById(id).populate("reviews");
-    res.render("campgrounds/show", { campground });
-  })
-);
+// Moved campgrounds routes to separate file
+app.use("/campgrounds", campgrounds);
 
 app.post(
   "/campgrounds/:id/reviews",
