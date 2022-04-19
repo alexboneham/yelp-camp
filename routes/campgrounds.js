cb56239469cdbb864b69a23b7bbdc1144ffb3f8d
Alexander Boneham <alexboneham@gmail.com>
2022-04-19 17:43:09 -0400

    Create basic authorization.
    Hide edit / delete buttons if not logged in.
    Add authorization check to campground PUT route (next make into middleware and add to all backend routes).

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -61,7 +61,12 @@ router.put(
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { new: true });
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+      req.flash('error', 'You do not have permission to do that!');
+      return res.redirect(`/campgrounds/${campground._id}`);
+    }
+    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { new: true });
     // Used spread here in vid, not sure why. Also don't need new: true
     req.flash('success', 'Successfully updated campground');
     res.redirect(`/campgrounds/${campground._id}`);
