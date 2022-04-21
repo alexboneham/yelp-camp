b460747afee0eb9a813b2a7ab0147daeb7fa3934
Alexander Boneham <alexboneham@gmail.com>
2022-04-20 21:17:02 -0400

    Set up cloudinary image upload.
    Configure cloudinary storage. Add Multer-storage-cloudinary package.
    Add Multer middleware to campground POST route.
    Access req.files in controllers, map over array and add image url/name to mongo db. 
    Loop over image array and display in show page.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,16 +5,13 @@ const { isLoggedIn } = require('../middleware');
 const { validateCampground, isAuthor } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 router
   .route('/')
   .get(catchAsync(campgrounds.index))
-  // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-  .post(upload.array('image'), (req, res) => {
-    console.log(req.body, req.files);
-    res.send("IT WORKED")
-  });
+  .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
