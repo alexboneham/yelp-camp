774ae207fdc66e30d4fa71608cb0ddbec0ba82aa
Alexander Boneham <alexboneham@gmail.com>
2022-04-22 15:52:19 -0400

    Fix bugs.
    Add geodata to seeds and reseed.
    Add conditional to campgrounds index page to handle case of no image.

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,14 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      //YOUR USER ID
       author: '625f23b32c73b4f448ea5ae5',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      geometry: {
+        type: 'Point',
+        coordinates: [-122.3301, 47.6038],
+      },
       images: [
         {
           url: 'https://res.cloudinary.com/dklocsnvf/image/upload/v1650551334/YelpCamp/nndlc0ti12xquosdu5kv.jpg',
@@ -33,7 +38,7 @@ const seedDB = async () => {
         {
           url: 'https://res.cloudinary.com/dklocsnvf/image/upload/v1650551335/YelpCamp/odfdhfs8xtdm451rosw6.jpg',
           filename: 'YelpCamp/odfdhfs8xtdm451rosw6',
-        }
+        },
       ],
       description:
         'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure culpa, sit provident, esse autem, repellat quidem vel quo libero ut cupiditate reiciendis vitae iste aut non ratione necessitatibus quis a.',
