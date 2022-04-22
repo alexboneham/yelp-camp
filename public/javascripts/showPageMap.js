647cfdf6eb7e0d7f983400dcbf08da85086b01b7
Alexander Boneham <alexboneham@gmail.com>
2022-04-22 15:18:00 -0400

    Load geo coordinates for mapbox from database and set marker.

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,9 @@
 mapboxgl.accessToken = mbxToken;
-  const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 5, // starting zoom
-  });
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/mapbox/streets-v11', // style URL
+  center: campground.geometry.coordinates, // starting position [lng, lat]
+  zoom: 11, // starting zoom
+});
+
+const marker = new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
